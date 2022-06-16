e28098839ce9acd76a884504e294784889f627cf
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-06-16 20:11:06 +0300

    added

---
+++ src/data.js
@@ -0,0 +1,296 @@
+export const navigation = [
+  {
+    name: 'home',
+    href: 'home',
+  },
+  {
+    name: 'about',
+    href: 'about',
+  },
+  {
+    name: 'features',
+    href: 'features',
+  },
+  {
+    name: 'contact',
+    href: 'contact',
+  },
+];
+
+export const hero = {
+  title: 'Creative Home Simpify your Furniture',
+  subtitle:
+    'Do i have consent to record this meeting gain locaion, root-and-branch, review, nor game plan who’s the goto',
+  buttonText: 'Show Now',
+};
+
+export const stats = [
+  {
+    experience: '7',
+  },
+  {
+    countries: '2',
+  },
+  {
+    sold: '10k+',
+  },
+  {
+    variants: '260+',
+  },
+];
+
+export const features = {
+  title: 'We Create your home more aestetic',
+  subtitle:
+    'Furnitre power is a software as services for multiperpose business management system',
+  buttonText: 'Show Now',
+  features: [
+    {
+      icon: '',
+      title: 'Valuation Services',
+      subtitle:
+        'Sometimes features require a short description.  This can be detailed description',
+    },
+    {
+      icon: '',
+      title: 'Development of Furniture Models',
+      subtitle:
+        'Sometimes features require a short description.  This can be detailed description',
+    },
+  ],
+  feature2: {
+    image: '',
+    title: 'The Best Furniture Manufacturer of your choice',
+    subtitle:
+      'Furnitre power is a software as services for multiperpose business management system, expecially for them who are running two or more business exploree the future Furnitre power is a software as services.',
+  },
+};
+
+export const newInStore = {
+  title: 'New In Store Now',
+  subtitle: 'Get the latest items immediately with promo prices',
+  link: 'Check all',
+  products: [
+    {
+      item: 'chair',
+      image: '',
+    },
+    {
+      item: 'bed',
+      image: '',
+    },
+    {
+      item: 'cupboard',
+      image: '',
+    },
+    {
+      item: 'lighting',
+      image: '',
+    },
+  ],
+};
+
+export const products = {
+  title: 'All Products',
+  subtitle:
+    'The products we provide only for you as our service are selected from the best products with number 1 quality in the world',
+  list: [
+    {
+      image: '',
+      icon: '',
+      name: 'Ceiling Light',
+      price: 75,
+      oldPrice: 82,
+    },
+    {
+      image: '',
+      icon: '',
+      name: 'Wood Chair',
+      price: 50,
+      oldPrice: 70,
+    },
+    {
+      image: '',
+      icon: '',
+      name: 'Papper Cupboard',
+      price: 105,
+      oldPrice: 120,
+    },
+    {
+      image: '',
+      icon: '',
+      name: 'Ole Gundorse Spring',
+      price: 75,
+      oldPrice: 82,
+    },
+    {
+      image: '',
+      icon: '',
+      name: 'Treos Seroes 911',
+      price: 200,
+      oldPrice: 210,
+    },
+    {
+      image: '',
+      icon: '',
+      name: 'Multi bilderman slibber',
+      price: 45,
+      oldPrice: 50,
+    },
+    {
+      image: '',
+      icon: '',
+      name: 'XORA corner desk',
+      price: 320,
+      oldPrice: 325,
+    },
+    {
+      image: '',
+      icon: '',
+      name: 'Black Forest Series Wood',
+      price: 225,
+      oldPrice: 240,
+    },
+  ],
+};
+
+export const testimonial = {
+  title: 'What people are saying about us',
+  image: '',
+  persons: [
+    {
+      image: '',
+      name: 'Josh Smith',
+      occupation: 'Manager of The New York Times',
+      message:
+        '“They are have a perfect touch for make something so professional ,interest and useful for a lot of people .”',
+    },
+    {
+      image: '',
+      name: 'Brandi Johns',
+      occupation: 'Manager of The New York Times',
+      message:
+        '“They are have a perfect touch for make something so professional ,interest and useful for a lot of people .”',
+    },
+    {
+      image: '',
+      name: 'Paula Pfeffer',
+      occupation: 'Manager of The New York Times',
+      message:
+        '“They are have a perfect touch for make something so professional ,interest and useful for a lot of people .”',
+    },
+  ],
+};
+
+export const newsletter = {
+  image: '',
+  title: 'Get more discount Off your order',
+  subtitle: 'Join our mailing list',
+  placeholder: 'Your email address',
+  buttonText: 'Shop Now',
+};
+
+export const footer = {
+  social: [
+    {
+      icon: '',
+    },
+    {
+      icon: '',
+    },
+    {
+      icon: '',
+    },
+    {
+      icon: '',
+    },
+  ],
+  linkSection: [
+    {
+      title: 'Our Products',
+      links: [
+        {
+          name: 'The Support Suite',
+        },
+        {
+          name: 'The Sales Suite',
+        },
+        {
+          name: 'Support',
+        },
+        {
+          name: 'Guide',
+        },
+      ],
+    },
+    {
+      title: 'Top Features',
+      links: [
+        {
+          name: 'Ticketing System',
+        },
+        {
+          name: 'Knowledge Base',
+        },
+        {
+          name: 'Community Forums',
+        },
+        {
+          name: 'Help Desk Software',
+        },
+      ],
+    },
+    {
+      title: 'Resources',
+      links: [
+        {
+          name: 'Product Support',
+        },
+        {
+          name: 'Request Demo',
+        },
+        {
+          name: 'Library',
+        },
+        {
+          name: 'Peoplepower Blog',
+        },
+      ],
+    },
+    {
+      title: 'Company',
+      links: [
+        {
+          name: 'About Us',
+        },
+        {
+          name: 'Press',
+        },
+        {
+          name: 'Investors',
+        },
+        {
+          name: 'Events',
+        },
+      ],
+    },
+    {
+      title: 'Favourite Things',
+      links: [
+        {
+          name: 'For Enterprise',
+        },
+        {
+          name: 'For Startups',
+        },
+        {
+          name: 'For Benchmark',
+        },
+        {
+          name: 'For Small Business',
+        },
+      ],
+    },
+  ],
+  copyright: 'FurniShop 2022 - All Rights Reserved.',
+};
