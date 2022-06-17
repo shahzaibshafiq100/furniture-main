6fbb5ac4320192c672e6f1d98a00a8a5ce403a3d
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-06-17 09:27:59 +0300

    added

--- src/data.js
+++ src/data.js
@@ -1,3 +1,37 @@
+// import icons
+import {
+  IoLogoYoutube,
+  IoLogoFacebook,
+  IoLogoGithub,
+  IoLogoInstagram,
+  IoMdAddCircle,
+  IoIosCheckmarkCircle,
+  IoIosArrowRoundForward,
+} from 'react-icons/io';
+
+// import images
+import Features1Img from './assets/img/features-1.png';
+import Features2Img from './assets/img/features-2.png';
+import ChairImg from './assets/img/chair.png';
+import BedImg from './assets/img/bed.png';
+import CupboardImg from './assets/img/cupboard.png';
+import LightingImg from './assets/img/lighting.png';
+import Product1Img from './assets/img/products/product-1.png';
+import Product2Img from './assets/img/products/product-2.png';
+import Product3Img from './assets/img/products/product-3.png';
+import Product4Img from './assets/img/products/product-4.png';
+import Product5Img from './assets/img/products/product-5.png';
+import Product6Img from './assets/img/products/product-6.png';
+import Product7Img from './assets/img/products/product-7.png';
+import Product8Img from './assets/img/products/product-8.png';
+import Product9Img from './assets/img/products/product-9.png';
+import Product10Img from './assets/img/products/product-10.png';
+import TestimonialImg from './assets/img/testimonial.png';
+import Avatar1Img from './assets/img/avatar-1.png';
+import Avatar2Img from './assets/img/avatar-2.png';
+import Avatar3Img from './assets/img/avatar-3.png';
+import Avatar4Img from './assets/img/avatar-4.png';
+
 export const navigation = [
   {
     name: 'home',
@@ -40,26 +74,27 @@ export const stats = [
 ];
 
 export const features = {
+  image: <Features1Img />,
   title: 'We Create your home more aestetic',
   subtitle:
     'Furnitre power is a software as services for multiperpose business management system',
   buttonText: 'Show Now',
-  features: [
+  items: [
     {
-      icon: '',
+      icon: <IoIosCheckmarkCircle />,
       title: 'Valuation Services',
       subtitle:
         'Sometimes features require a short description.  This can be detailed description',
     },
     {
-      icon: '',
+      icon: <IoIosCheckmarkCircle />,
       title: 'Development of Furniture Models',
       subtitle:
         'Sometimes features require a short description.  This can be detailed description',
     },
   ],
   feature2: {
-    image: '',
+    image: <Features2Img />,
     title: 'The Best Furniture Manufacturer of your choice',
     subtitle:
       'Furnitre power is a software as services for multiperpose business management system, expecially for them who are running two or more business exploree the future Furnitre power is a software as services.',
@@ -70,22 +105,23 @@ export const newInStore = {
   title: 'New In Store Now',
   subtitle: 'Get the latest items immediately with promo prices',
   link: 'Check all',
+  icon: <IoIosArrowRoundForward />,
   products: [
     {
-      item: 'chair',
-      image: '',
+      name: 'chair',
+      image: <ChairImg />,
     },
     {
-      item: 'bed',
-      image: '',
+      name: 'bed',
+      image: <BedImg />,
     },
     {
-      item: 'cupboard',
-      image: '',
+      name: 'cupboard',
+      image: <CupboardImg />,
     },
     {
-      item: 'lighting',
-      image: '',
+      name: 'lighting',
+      image: <LightingImg />,
     },
   ],
 };
@@ -97,83 +133,97 @@ export const products = {
   list: [
     {
       image: '',
-      icon: '',
+      icon: <IoMdAddCircle />,
       name: 'Ceiling Light',
       price: 75,
       oldPrice: 82,
     },
     {
       image: '',
-      icon: '',
+      icon: <IoMdAddCircle />,
       name: 'Wood Chair',
       price: 50,
       oldPrice: 70,
     },
     {
       image: '',
-      icon: '',
+      icon: <IoMdAddCircle />,
       name: 'Papper Cupboard',
       price: 105,
       oldPrice: 120,
     },
     {
       image: '',
-      icon: '',
+      icon: <IoMdAddCircle />,
       name: 'Ole Gundorse Spring',
       price: 75,
       oldPrice: 82,
     },
     {
       image: '',
-      icon: '',
+      icon: <IoMdAddCircle />,
       name: 'Treos Seroes 911',
       price: 200,
       oldPrice: 210,
     },
     {
       image: '',
-      icon: '',
+      icon: <IoMdAddCircle />,
       name: 'Multi bilderman slibber',
       price: 45,
       oldPrice: 50,
     },
     {
       image: '',
-      icon: '',
+      icon: <IoMdAddCircle />,
       name: 'XORA corner desk',
       price: 320,
       oldPrice: 325,
     },
     {
       image: '',
-      icon: '',
+      icon: <IoMdAddCircle />,
       name: 'Black Forest Series Wood',
       price: 225,
       oldPrice: 240,
+    },
+    {
+      image: '',
+      icon: <IoMdAddCircle />,
+      name: 'Papper Cupboard',
+      price: 105,
+      oldPrice: 120,
+    },
+    {
+      image: '',
+      icon: <IoMdAddCircle />,
+      name: 'Ole Gundorse Spring',
+      price: 75,
+      oldPrice: 82,
     },
   ],
 };
 
 export const testimonial = {
   title: 'What people are saying about us',
-  image: '',
+  image: <TestimonialImg />,
   persons: [
     {
-      image: '',
+      image: <Avatar1Img />,
       name: 'Josh Smith',
       occupation: 'Manager of The New York Times',
       message:
         '“They are have a perfect touch for make something so professional ,interest and useful for a lot of people .”',
     },
     {
-      image: '',
+      image: <Avatar2Img />,
       name: 'Brandi Johns',
       occupation: 'Manager of The New York Times',
       message:
         '“They are have a perfect touch for make something so professional ,interest and useful for a lot of people .”',
     },
     {
-      image: '',
+      image: <Avatar3Img />,
       name: 'Paula Pfeffer',
       occupation: 'Manager of The New York Times',
       message:
@@ -183,7 +233,6 @@ export const testimonial = {
 };
 
 export const newsletter = {
-  image: '',
   title: 'Get more discount Off your order',
   subtitle: 'Join our mailing list',
   placeholder: 'Your email address',
@@ -193,16 +242,16 @@ export const newsletter = {
 export const footer = {
   social: [
     {
-      icon: '',
+      icon: <IoLogoYoutube />,
     },
     {
-      icon: '',
+      icon: <IoLogoInstagram />,
     },
     {
-      icon: '',
+      icon: <IoLogoGithub />,
     },
     {
-      icon: '',
+      icon: <IoLogoFacebook />,
     },
   ],
   linkSection: [
