5427759ff0dd9a4f46e54b9074eb2b3e02d5b416
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-06-19 10:18:24 +0300

    added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 // import logo
 import Logo from '../assets/img/logo.svg';
@@ -6,14 +6,24 @@ import Logo from '../assets/img/logo.svg';
 import { CgMenuRight } from 'react-icons/cg';
 
 const Header = () => {
+  const [bg, setBg] = useState(false);
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      return window.scrollY > 50 ? setBg(true) : setBg(false);
+    });
+  });
   return (
-    <header className='fixed py-8 z-10 w-full'>
+    <header
+      className={`${
+        bg ? 'bg-primary py-4 lg:py-6' : 'bg-none'
+      } fixed left-0 py-8 z-10 w-full transition-all duration-200`}
+    >
       <div className='container mx-auto'>
         <div className='flex justify-between items-center'>
           {/* logo */}
-          <img src={Logo} alt='' />
+          <img className='h-6 lg:h-8' src={Logo} alt='' />
           {/* menu icon */}
-          <div className='text-2xl text-white'>
+          <div className='text-2xl lg:text-3xl text-white'>
             <CgMenuRight />
           </div>
         </div>
