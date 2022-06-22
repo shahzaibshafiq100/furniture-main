bba22238bfc94391412c13077324417d6659b429
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-06-22 19:45:27 +0300

    added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import Features from './components/Features';
-import FeaturesSecond from './components/FeaturesSecond';
 
 // import components
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Features from './components/Features';
+import FeaturesSecond from './components/FeaturesSecond';
 import NewItems from './components/NewItems';
 import Products from './components/Products';
 import Testimonial from './components/Testimonial';
+import Newsletter from './components/Newsletter';
 
 const App = () => {
   return (
-    <div className='w-full max-w-[1440px] h-[5000px] mx-auto bg-white'>
+    <div className='w-full max-w-[1440px] mx-auto bg-white'>
       <Header />
       <Hero />
       <Features />
@@ -19,6 +20,7 @@ const App = () => {
       <FeaturesSecond />
       <Products />
       <Testimonial />
+      <Newsletter />
     </div>
   );
 };
