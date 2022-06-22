70eb0346a9fc859f44a7021be83f3376b63e2b70
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-06-22 22:58:59 +0300

    added

--- src/components/NewItemsSlider.js
+++ src/components/NewItemsSlider.js
@@ -9,35 +9,33 @@ import { newInStore } from '../data';
 
 const NewItemsSlider = () => {
   return (
-    <>
-      <Swiper
-        grabCursor={true}
-        breakpoints={{
-          320: {
-            slidesPerView: 2,
-            spaceBetween: 18,
-          },
-          768: {
-            slidesPerView: 3,
-            spaceBetween: 20,
-          },
-        }}
-        className='mySwiper'
-      >
-        {newInStore.products.map((product, index) => {
-          return (
-            <SwiperSlide className='max-w-[265px]' key={index}>
-              <div className='relative'>
-                <img src={product.image.type} alt='' />
-                <div className='absolute text-white bottom-[20px] text-center w-full text-[18px] lg:text-2xl font-medium capitalize'>
-                  {product.name}
-                </div>
+    <Swiper
+      grabCursor={true}
+      breakpoints={{
+        320: {
+          slidesPerView: 2,
+          spaceBetween: 18,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+      }}
+      className='mySwiper'
+    >
+      {newInStore.products.map((product, index) => {
+        return (
+          <SwiperSlide className='max-w-[265px]' key={index}>
+            <div className='relative'>
+              <img src={product.image.type} alt='' />
+              <div className='absolute text-white bottom-[20px] text-center w-full text-[18px] lg:text-2xl font-medium capitalize'>
+                {product.name}
               </div>
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
-    </>
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
   );
 };
 
